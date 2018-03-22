dcafcfd4a5b11789bbb71d07c5ea562ba748f453
Martin Passard <martin.passard@etudiant.univ-lille1.fr>
2018-03-22 08:23:17 +0100

    Information Dto

---
+++ common/src/main/java/fr/iutinfo/skeleton/common/dto/InformationDto.java
@@ -0,0 +1,34 @@
+package fr.iutinfo.skeleton.common.dto;
+
+public class InformationDto {
+	private int id_user;
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String numero;
+	private String statut;
+	
+	private String getNom() {
+		return nom;
+	}
+	
+	private String getPrenom() {
+		return prenom;
+	}
+	
+	private String getMail() {
+		return mail;
+	}
+	
+	private String getNumero() {
+		return numero;
+	}
+	
+	private String getStatus() {
+		return status;
+	}
+	
+	
+	
+	
+}
