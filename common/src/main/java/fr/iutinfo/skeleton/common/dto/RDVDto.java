e11346a8cf7d0645c3eb9b3476d6a297ee8d630d
Cyril Bouchery <cyril.bouchery@etudiant.univ-lille1.fr>
2018-03-22 08:47:40 +0100

    RDV Dto

---
+++ common/src/main/java/fr/iutinfo/skeleton/common/dto/RDVDto.java
@@ -0,0 +1,51 @@
+package fr.iutinfo.skeleton.common.dto;
+
+import java.sql.Time;
+
+public class RDVDto {
+	//final static Logger logger = LoggerFactory.getLogger(RDVDto.class);
+	private int id_senior ;
+	private Time jour;
+	private boolean matin;
+	private boolean aprem;
+	private boolean soir;
+	private String libelle;
+	public int getId_senior() {
+		return id_senior;
+	}
+	public void setId_senior(int id_senior) {
+		this.id_senior = id_senior;
+	}
+	public Time getJour() {
+		return jour;
+	}
+	public void setJour(Time jour) {
+		this.jour = jour;
+	}
+	public boolean getMatin() {
+		return matin;
+	}
+	public void setMatin(boolean matin) {
+		this.matin = matin;
+	}
+	public boolean getAprem() {
+		return aprem;
+	}
+	public void setAprem(boolean aprem) {
+		this.aprem = aprem;
+	}
+	public boolean getSoir() {
+		return soir;
+	}
+	public void setSoir(boolean soir) {
+		this.soir = soir;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+}
