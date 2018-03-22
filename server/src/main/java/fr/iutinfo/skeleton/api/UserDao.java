800eb88c52ac2c288227569297494c5f4a8bbddd
Patrick BROGNIART <brogniart.p@gmail.com>
2018-03-22 09:06:05 +0100

    user userDao userDto

--- server/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
@@ -7,20 +7,20 @@ import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 import java.util.List;
 
 public interface UserDao {
-    @SqlUpdate("create table users (id integer primary key autoincrement, name varchar(100), alias varchar(100), email varchar(100), passwdHash varchar(64), salt varchar(64), search varchar(1024))")
+    @SqlUpdate("create table users (id_user integer primary key autoincrement, login varchar(100), mdp varchar(64), salt varchar(64))")
     void createUserTable();
 
-    @SqlUpdate("insert into users (name,alias,email, passwdHash, salt, search) values (:name, :alias, :email, :passwdHash, :salt, :search)")
+    @SqlUpdate("insert into users (login, mdp, salt) values (:login, :mdp, :salt)")
     @GetGeneratedKeys
     int insert(@BindBean() User user);
 
-    @SqlQuery("select * from users where name = :name")
+    @SqlQuery("select * from users where login = :login")
     @RegisterMapperFactory(BeanMapperFactory.class)
-    User findByName(@Bind("name") String name);
-
-    @SqlQuery("select * from users where search like :name")
+    User findByLogin(@Bind("login") String login);
+    
+    @SqlQuery("select * from users where id = :id")
     @RegisterMapperFactory(BeanMapperFactory.class)
-    List<User> search(@Bind("name") String name);
+    User findById(@Bind("id") int id);
 
     @SqlUpdate("drop table if exists users")
     void dropUserTable();
@@ -31,10 +31,6 @@ public interface UserDao {
     @SqlQuery("select * from users order by id")
     @RegisterMapperFactory(BeanMapperFactory.class)
     List<User> all();
-
-    @SqlQuery("select * from users where id = :id")
-    @RegisterMapperFactory(BeanMapperFactory.class)
-    User findById(@Bind("id") int id);
 
     void close();
 }
