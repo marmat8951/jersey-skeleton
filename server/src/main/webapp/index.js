c2e17bca2ba9d9feffd3ae9a1cd8b0cef3112cbb
Patrick BROGNIART <brogniart.p@gmail.com>
2018-03-23 15:07:06 +0100

    inscription complete

--- server/src/main/webapp/index.js
+++ server/src/main/webapp/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         $('#connexion').hide();
         $('#deconnexion').show();
         $('#onglet').show();
-    document.body.style.backgroundColor = "#354665";
+        document.body.style.backgroundColor = "#354665";
     });
 
     $('#logo').click(function() {
@@ -42,7 +42,7 @@ ong1 = document.getElementById('inscription_login_page');
 ong2 = document.getElementById('connexion_login_page');
 contenu = document.getElementById('content_login_page');
 
-const identificationPage = "<div class='container'><div class='row centered-form'><div class='col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4'><div class='panel'><div class='panel-body'><div class='form-group'><input type='text' name='login' id='login' class='form-control input-sm' placeholder='login'></div><div class='form-group'><input type='password' name='pass' id='pass' class='form-control input-sm' placeholder='password'></div><input type='submit' value='Connexion' class='btn btn-info btn-block'></div></div></div></div></div>";
+const identificationPage = "<div class='container'><div class='row centered-form'><div class='col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4'><div class='panel'><div class='panel-body'><div class='form-group'><input type='text' name='login' id='login' class='form-control input-sm' placeholder='login'></div><div class='form-group'><input type='password' name='pass' id='pass' class='form-control input-sm' placeholder='password'></div><button type='button' onClick='connexion()' name='buttonConnexion' class='btn btn-info btn-block'>Connexion</button></div></div></div></div></div>";
 
 const inscriptionPage = "<div class='container'><div class='row centered-form'><div class='col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4'><div class='panel'><div class='panel-body'><div class='row'><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><input type='text' name='first_name' id='first_name' class='form-control input-sm floatlabel' placeholder='First Name'></div></div><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><input type='text' name='last_name' id='last_name' class='form-control input-sm' placeholder='Last Name'></div></div></div><div class='row'><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><input type='text' name='numero' id='numero' class='form-control input-sm floatlabel' placeholder='Number'></div></div><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><select name='type' id='type' class='form-control'><option>etudiant</option><option>senior</option></select></div></div></div><div class='form-group'><input type='email' name='email' id='email' class='form-control input-sm' placeholder='Email Address'></div><div class='form-group'><input type='text' name='login' id='login' class='form-control input-sm' placeholder='Login'></div><div class='row'><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><input type='password' name='password' id='password' class='form-control input-sm' placeholder='Password'></div></div><div class='col-xs-6 col-sm-6 col-md-6'><div class='form-group'><input type='password' name='password_confirmation' id='password_confirmation' class='form-control input-sm' placeholder='Confirm Password'></div></div></div><button type='button' onClick='inscription()' name='buttonInscription' class='btn btn-info btn-block'>Inscription</button></div></div></div></div></div>";
 contenu.innerHTML = identificationPage;
@@ -72,12 +72,12 @@ ong2.addEventListener("click",function(){
 function inscription(){
     var nom=document.getElementById("last_name").value;
     var prenom=document.getElementById("first_name").value;
-    var telephone=document.getElementById("numero").value;
+    var numero=document.getElementById("numero").value;
     var login=document.getElementById("login").value;
-    var mail=document.getElementById("email").value;
+    var email=document.getElementById("email").value;
     var mdp=document.getElementById("password").value;
     var planning=null;
-    var type=document.getElementById("type").value;
+    var statut=document.getElementById("type").value;
 
     <!--changer le type en menu deroulant (senior/etudiant)-->
         var url="v1/user/"; 
@@ -87,11 +87,12 @@ function inscription(){
         url : url,
         dataType : "json",
         data : JSON.stringify({
-            "name" : nom,
-            "alias" : login,
-            "email" : mail,
-            "password" : mdp,
-            "id" : 0
+            "nom" : nom,
+            "prenom" : prenom,
+            "login" : login,
+            "numero" : numero,
+            "statut" : statut,
+            "email" : email
         }),
         success : function(data, textStatus, jqXHR) {
             alert("Utilisateur créé");
@@ -112,20 +113,22 @@ function connexion(){
     var login=document.getElementById("login").value;
     var mdp=document.getElementById("pass").value;
 
-    <!--changer le type en menu deroulant (senior/etudiant)-->
-        var url="v1/user/"+login; 
+
+    var url="v1/user/"+login; 
     $.ajax({
         type : 'GET',
         url : url,
         dataType : "json",
-        success : function(data, textStatus, jqXHR) {
+        success : function(json) {
+
+            document.body.style.backgroundColor = "white";
             var tab = JSON.stringify(json);
             var js=JSON.parse(tab);
-                alert("Connexion OK !");
-                $('#conteneurAccueil').show();
-                $('#deconnexion').show();
-                $('#onglet').hide();
-          
+            alert("Connexion OK !");
+            $('#conteneurAccueil').show();
+            $('#deconnexion').show();
+            $('#onglet').hide();
+
         },
         error : function(jqXHR, textStatus, errorThrown) {
             alert("Login non valide !")
