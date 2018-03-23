7767bd313d6d00c9a89affbdf8ddf8b74e6c7156
Jordao_Ninon <ninon.jordao@etudiant.univ-lille1.fr>
2018-03-23 10:55:57 +0100

    fix: Build

--- server/src/main/java/fr/iutinfo/skeleton/api/UserResource.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/UserResource.java
@@ -5,6 +5,7 @@ import static fr.iutinfo.skeleton.api.BDDFactory.tableExist;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -13,6 +14,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
