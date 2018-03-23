b6136c4ed4087f11717dea43c94a7c3497c0cc44
Jordao_Ninon <ninon.jordao@etudiant.univ-lille1.fr>
2018-03-23 08:33:27 +0100

    dispo

--- server/src/main/java/fr/iutinfo/skeleton/api/DispoDao.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/DispoDao.java
@@ -12,32 +12,32 @@ import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 
 public interface DispoDao {
 	
-	 @SqlUpdate("create table disponibilité (id_user int not null" + 
-	 		"       	     		   jour text,\n" + 
-	 		"			   matin boolean,\n" + 
+	@SqlUpdate("create table dispo(id_user serial primary key,\n" + 
+    		"       	         jour text)") void createDispoTable();
+	 	/*	"			   matin boolean,\n" + 
 	 		"			   aprem boolean,\n" + 
 	 		"			   soir boolean,\n" + 
 	 		"			   foreign key(id_user) references users(id_user),\n" + 
-	 		"			   constraint pk_dispo primary key(id_user,jour))")
-	   	void createDispoTable(); 
+	 		"			   constraint pk_dispo primary key(id_user,jour))")*/
+	   	
 	 
-	 @SqlUpdate("insert into disponibilité (id_user, jour, matin, aprem, soir) values (:id_user, :jour, :matin, :aprem, :soir)")
+	 @SqlUpdate("insert into dispo (id_user, jour) values (:id_user, :jour)")
 	    @GetGeneratedKeys
 	    int insert(@BindBean() Dispo dispo);
 	 
-	 @SqlQuery("select * from disponibilité where id_user = :id_user")
+	 @SqlQuery("select * from dispo where id_user = :id_user")
 	    @RegisterMapperFactory(BeanMapperFactory.class)
 	    Dispo findByIdUser(@Bind("id_user") int id);
 	    
-	    @SqlUpdate("drop table if exists disponibilité")
+	    @SqlUpdate("drop table if exists dispo")
 	    void dropDispoTable();
 	    
 	    
 	    
-	    @SqlUpdate("delete from disponibilité where id_user = :id_user")
+	    @SqlUpdate("delete from dispo where id_user = :id_user")
 	    void delete(@Bind("id_user") int id);
 	    
-	    @SqlQuery("select * from disponibilité order by id_user")
+	    @SqlQuery("select * from dispo order by id_user")
 	    @RegisterMapperFactory(BeanMapperFactory.class)
 	    List<Dispo> all();
 	    
