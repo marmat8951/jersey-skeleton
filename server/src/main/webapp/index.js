4e748a074387ea4e70b2f74d15e19d85b004a982
Patrick BROGNIART <brogniart.p@gmail.com>
2018-03-26 16:43:24 +0200

    verif mail inscription

--- server/src/main/webapp/index.js
+++ server/src/main/webapp/index.js
@@ -106,8 +106,10 @@ function inscription(){
         var planning=null;
         var statut=document.getElementById("type").value;
 
-        var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-        if(!expressionReguliere.test(login)){
+
+        var regEmail = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i');
+
+        if(!regEmail.test(login)){
 
             alert("Addresse mail incorrect !");
             $('login').css('border','3px solid red'); 
