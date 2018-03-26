fec75b5ac1e7b9ea149a8d05f0f5cae61f11b301
Jordao_Ninon <ninon.jordao@etudiant.univ-lille1.fr>
2018-03-26 08:22:35 +0200

    Liste hobbies

---
+++ common/src/main/java/fr/iutinfo/skeleton/common/dto/ListHobbieDto.java
@@ -0,0 +1,22 @@
+package fr.iutinfo.skeleton.common.dto;
+
+public class ListHobbieDto {
+	private int id_user; 
+	private String id_hob;
+	
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+	public String getId_hob() {
+		return id_hob;
+	}
+	public void setId_hob(String id_hob) {
+		this.id_hob = id_hob;
+	}
+	
+	
+	
+}
