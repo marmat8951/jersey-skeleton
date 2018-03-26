badce141c64dc7a0b8b810d7607fa12d5459de80
Jordao_Ninon <ninon.jordao@etudiant.univ-lille1.fr>
2018-03-26 08:37:27 +0200

    List Hobbie DAO

--- server/src/main/java/fr/iutinfo/skeleton/api/ListHobbieDao.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/ListHobbieDao.java
@@ -1,5 +1,40 @@
 package fr.iutinfo.skeleton.api;
 
-public class ListHobbieDao {
+import java.util.List;
 
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+public interface ListHobbieDao {
+    @SqlUpdate("create table list_hobbies(id_user int not null, id_hob text not null, " 
+    		+ " foreign key(id_hob) references hobbies(libelle), foreign key(id_user) references users(id_user)"
+    		+ "constraint pk_list_hob primary key(id_user,id_hob))") void createListHobbiesTable();
+    	
+
+    
+    @SqlUpdate("insert into list_hobbies (id_user,id_hob ) values (:id_user, :id_hob)")
+    @GetGeneratedKeys
+    int insert(@BindBean() RDV rdv);
+
+    @SqlQuery("select * from list_hobbies where id_user = :id_user")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    ListHobbie findByIdUser(@Bind("id_user") int id);
+    
+    @SqlUpdate("drop table if exists list_hobbies")
+    void dropRDVTable();
+    
+    
+    @SqlUpdate("delete from list_hobbies where id_senior = :id_senior AND id_hob = :id_hob")
+    void delete(@Bind("id_senior") int id);
+    
+    @SqlQuery("select * from list_hobbies order by id_user")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<ListHobbie> all();
+    
+    void close();
 }
