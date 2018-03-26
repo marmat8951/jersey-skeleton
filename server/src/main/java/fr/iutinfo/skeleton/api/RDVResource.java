09670727ac9a402b7c9fe9f4d22c29d25e5e7aec
Cyril Bouchery <cyril.bouchery@etudiant.univ-lille1.fr>
2018-03-26 09:02:35 +0200

    maj classes rdv

--- server/src/main/java/fr/iutinfo/skeleton/api/RDVResource.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/RDVResource.java
@@ -4,8 +4,6 @@ import static fr.iutinfo.skeleton.api.BDDFactory.getDbi;
 import static fr.iutinfo.skeleton.api.BDDFactory.tableExist;
 
 import java.sql.SQLException;
-import java.sql.Time;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,10 +20,9 @@ import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.iutinfo.skeleton.common.dto.DispoDto;
 import fr.iutinfo.skeleton.common.dto.RDVDto;
 
-@Path("/RDV")
+@Path("/rdv")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class RDVResource {
@@ -43,29 +40,15 @@ public class RDVResource {
     public RDVDto createRDV(RDVDto dto) {
         RDV rdv = new RDV();
         rdv.initFromDto(dto);
-        int id = dao.insert(rdv);
-        dto.setId_senior(id);
+        dao.insert(rdv);
+        //dto.setSenior(id);
         return dto;
     }
     
     @GET
-	@Path("/test")
-	public RDVDto rdvTest() {
-		RDVDto dto = new RDVDto();
-		dto.setAprem(true);
-		dto.setId_etu(1);
-		dto.setId_senior(1);
-		dto.setJour("lundi");
-		dto.setLibelle("conduite");
-		dto.setMatin(true);
-		dto.setSoir(false);
-		return dto;
-	}
-    
-    @GET
-    @Path("/{id_senior}")
-    public RDVDto getRDV(@PathParam("id_senior") int id_senior) {
-        RDV rdv = dao.findByIdSenior(id_senior);
+    @Path("/{senior}")
+    public RDVDto getRDV(@PathParam("id_senior") String senior) {
+        RDV rdv = dao.findBySenior(senior);
         if (rdv == null) {
             throw new WebApplicationException(404);
         }
@@ -80,8 +63,8 @@ public class RDVResource {
     }
     
     @DELETE
-    @Path("/{id_senior}")
-    public void deleteUser(@PathParam("id_senior") int id) {
+    @Path("/{senior}")
+    public void deleteUser(@PathParam("senior") String id) {
         dao.delete(id);
     }
 }
